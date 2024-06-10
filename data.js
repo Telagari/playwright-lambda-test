e6769508bd913a1b018bdb1c4367c0a62f9b2e90
sudhakar <Telagari.sudhakar@trinet.com>
2024-06-10 21:27:22 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,19 @@
+export const data = {
+ 
+    URL: "https://www.lambdatest.com/selenium-playground/",
+    WELCOME_MSG: "Welcome to LambdaTest",
+    USER_NAME: "Telagari Sudhakar",
+    EMAIL: "sudhakartelagari03@gmail.com",
+    PASSWORD: "Test@123",
+    COMPANY: "coforge",
+    WEBSITE: "www.test.com",
+    COUNTRY: "India",
+    CITY: "Hyderabad",
+    ADDRESS1: "Rudravaram",
+    ADDRESS2: "Beside market",
+    STATE: "AP",
+    ZIP: "518594",
+    SUCCESS_MSG: "Thanks for contacting us, we will get back to you shortly."
+   
+   
+  }
